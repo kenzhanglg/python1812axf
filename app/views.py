6683de7d5a39650e8033e66ca61b6ec6704f97e4
kenzhang <18814187940@163.com>
2019-03-09 12:20:39 +0800

    我的部分

--- app/views.py
+++ app/views.py
@@ -34,7 +34,7 @@ def home(request):
     return render(request,'home/home.html',context=response_dir)
 
 # def market(request,categoryid=104749,):
-def market(request):
+def market(request,childid='0',sortid='0'):
 
     foodtypes = Foodtype.objects.all()
     #商品信息
@@ -45,11 +45,38 @@ def market(request):
 
     index= int(request.COOKIES.get('index','0'))
     categoryid = foodtypes[index].typeid
-    goods_list = Goods.objects.filter(categoryid=categoryid)
+    # goods_list = Goods.objects.filter(categoryid=categoryid)
+    if childid == '0':
+        goods_list = Goods.objects.filter(categoryid=categoryid)
+    else:
+        goods_list = Goods.objects.filter(childcid=childid)
+
+    if sortid == '1':
+        goods_list = goods_list.order_by('-productnum')
+    elif sortid == '2':
+        goods_list = goods_list.order_by('price')
+    elif sortid == '3':
+        goods_list = goods_list.order_by('-price')
+
+
+    childtypenames = foodtypes[index].childtypenames
+    childtype_list = []
+    for item in childtypenames.split('#'):
+        item_arr = item.split(':')
+        temp_arr = {
+            'name':item_arr[0],
+            'id':item_arr[1],
+        }
+        childtype_list.append(temp_arr)
+
+
+
+
     response_str = {
         'foodtypes':foodtypes,
         'goods_list':goods_list,
-
+        'childtype_list':childtype_list,
+        'childid':childid,
     }
     return render(request,'market/market.html',context=response_str)
 
--- static/market/js/market.js
+++ static/market/js/market.js
@@ -21,5 +21,66 @@ $(function () {
 
     })
 
+       // 子类
+    var categoryShow = false
+    $('#category-bt').click(function () {
+        // console.log(categoryShow)
+        // if (categoryShow){  // 隐藏
+        //     categoryShow = false
+        //     $('.category-view').hide()
+        // } else { // 显示
+        //     categoryShow = true
+        //     $('.category-view').show()
+        // }
+
+        // 取反
+        categoryShow = !categoryShow
+        categoryShow ? categoryViewShow() : categoryViewHide()
+        console.log('子类点击')
+    })
+
+    function categoryViewShow() {
+        $('.category-view').show()
+        $('#category-bt i').removeClass('glyphicon-chevron-up').addClass('glyphicon-chevron-down')
+
+        sortViewHide()
+        sortShow = false
+    }
+
+    function categoryViewHide() {
+        $('.category-view').hide()
+        $('#category-bt i').removeClass('glyphicon-chevron-down').addClass('glyphicon-chevron-up')
+    }
+
+    // 排序
+    var sortShow = false
+    $('#sort-bt').click(function () {
+        sortShow = !sortShow
+        sortShow ? sortViewShow() : sortViewHide()
+        console.log('排序点击')
+    })
+
+    function sortViewShow() {
+        $('.sort-view').show()
+        $('#sort-bt i').removeClass('glyphicon-chevron-up').addClass('glyphicon-chevron-down')
+
+        categoryViewHide()
+        categoryShow = false
+    }
+
+    function sortViewHide() {
+        $('.sort-view').hide()
+        $('#sort-bt i').removeClass('glyphicon-chevron-down').addClass('glyphicon-chevron-up')
+    }
+
+    // 灰色蒙层
+    $('.bounce-view').click(function () {
+        sortViewHide()
+        sortShow = false
+
+        categoryViewHide()
+        categoryShow = false
+    })
+
 
 })
